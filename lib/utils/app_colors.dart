00cab44466419184282c164e880bd2abeebe29b7
Homework <allamurodovravshan38@gmail.com>
2023-06-07 02:49:03 +0500

    ads

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color C_6C7072 = Color(0xFF6C7072);
+  static const Color C_22222A = Color(0xFF22222A);
+  static const Color C_E1D24A_1 = Color(0xFFE1D24A);
+  static const Color C_C69233_2 = Color(0xFFC69233);
+  static const Color C_CDD2DE = Color(0xFFCDD2DE);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color C_151418 = Color(0xFF151418);
+  static const Color C_6A6A6E = Color(0xFF6A6A6E);
+  static const Color C_19191D = Color(0xFF19191D);
+  static const Color black = Color(0xFF000000);
+}
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,21 @@
+class AppImages {
+  // images
+  static const burger = "assets/images/burger.png";
+  static const disert = "assets/images/disert.png";
+  static const facebookIcon = "assets/images/facebook_icon.png";
+  static const googleIcon = "assets/images/google_icon.png";
+  static const free = "assets/images/free.png";
+
+  // svg
+  static const arrowBack = "assets/svg/arrow_back.svg";
+  static const eye = "assets/svg/eye.svg";
+  static const home = "assets/svg/home.svg";
+  static const like = "assets/svg/like.svg";
+  static const minus = "assets/svg/minus.svg";
+  static const passwordIcon = "assets/svg/password_icon.svg";
+  static const plus = "assets/svg/plus.svg";
+  static const profile = "assets/svg/profile.svg";
+  static const send = "assets/svg/send.svg";
+  static const zakas = "assets/svg/zakas.svg";
+  static const zakas2 = "assets/svg/zakas2.svg";
+}
