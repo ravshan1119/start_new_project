d6f256e59dfb701d6b94c083d4b0aad249fd9985
Homework <allamurodovravshan38@gmail.com>
2023-06-07 02:20:42 +0500

    ads

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+
+      ),
+    );
   }
 }
